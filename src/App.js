4f44959ee793b69ab4d9cdd5aa87bfd62a55791a
ktotheH <kcjhill1234@msn.com>
2020-09-07 08:26:20 -0600

    components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import {Header, Table, TableBody, TableHeader} from "semantic-ui-react"
 
 function App() {
   return (
     <div className="App">
 <h1>Hello, this is React.</h1>
-<Header/>
-<Table>
-  <TableHeader>EmployeeDirectory</TableHeader>
-  <TableBody>
-<p>insert table here</p>
-  </TableBody>
-</Table>
-
     </div>
   );
 }
