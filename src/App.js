97b8d08f2f2851ccd65fe606f936410746987e7c
ktotheH <kcjhill1234@msn.com>
2020-09-06 20:34:45 -0600

    Starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Header, Table, TableBody, TableHeader} from "semantic-ui-react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Hello, this is React.</h1>
+<Header/>
+<Table>
+  <TableHeader>EmployeeDirectory</TableHeader>
+  <TableBody>
+<p>insert table here</p>
+  </TableBody>
+</Table>
+
     </div>
   );
 }
